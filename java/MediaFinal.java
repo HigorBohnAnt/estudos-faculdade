4f5a2b9a40ee225237794a2edf88837dedcc13a9
HigorBohnAnt <higorbohn@gmail.com>
2026-09-05 18:48:44 -0300

    Adicionar classe MediaFinal para calcular e exibir a média de duas notas

---
+++ java/MediaFinal.java
@@ -0,0 +1,8 @@
+public class MediaFinal {
+    public static void main(String[] args){
+        double nota1 = 7.0;
+        double nota2 = 8.0;
+        double media = (nota1 + nota2) / 2;
+        System.out.println("Média final: " + media);
+    }
+}
